6030c81992b02195bc37c4588b1475583cea09a2
Kai Foday <kaifoday25@.com>
2026-09-21 13:27:10 +0000

    added frontend url and vercel.json

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://webgen-frontend-ai.vercel.app",
   credentials: true
 }));
 app.use(express.json({ limit: "1mb" }));
